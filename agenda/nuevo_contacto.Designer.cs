234fe10849db793bd816c64024396c38bc67eb31
aleramirezsj <aleramirezsj@gmail.com>
2013-09-07 08:41:15 -0300

    Incorporando la agenda y el Script para poder replicar la base de datos en los equipos de los alumnos para que puedan hacer las pruebas en su casa. De Agenda incorporamos insertar_contacto utilizando procedimientos almacenados y parametros del lado de C#

---
+++ agenda/nuevo_contacto.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace agenda
+{
+    partial class nuevo_contacto
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtdni = new System.Windows.Forms.TextBox();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(53, 215);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(75, 23);
+            this.btnguardar.TabIndex = 0;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Apellido";
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(83, 39);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(148, 20);
+            this.txtapellido.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DNI.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(83, 81);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(148, 20);
+            this.txtnombre.TabIndex = 5;
+            // 
+            // txtdni
+            // 
+            this.txtdni.Location = new System.Drawing.Point(83, 125);
+            this.txtdni.Name = "txtdni";
+            this.txtdni.Size = new System.Drawing.Size(148, 20);
+            this.txtdni.TabIndex = 6;
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.Location = new System.Drawing.Point(156, 215);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(75, 23);
+            this.btncancelar.TabIndex = 7;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = true;
+            // 
+            // nuevo_contacto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.txtdni);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtapellido);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnguardar);
+            this.Name = "nuevo_contacto";
+            this.Text = "Nuevo contacto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtdni;
+        private System.Windows.Forms.Button btncancelar;
+    }
+}
+
---
+++ agenda/nuevo_contacto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace agenda
+{
+    public partial class nuevo_contacto : Form
+    {
+        public nuevo_contacto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            //creamos el objeto que establece la conexión con el 
+            //sql server
+            SqlConnection miConexion = new SqlConnection();
+            miConexion.ConnectionString = "Data source=NBRAR\\SQLEXPRESS2008";
+            miConexion.ConnectionString += ";Initial catalog=agenda_seminario_2013";
+            miConexion.ConnectionString += ";User Id=sa";
+            miConexion.ConnectionString += ";Password=123";
+            miConexion.Open();
+
+            //creamos el objeto que nos permitirá enviar comandos 
+            //al servidor
+            SqlCommand miComando = new SqlCommand();
+            miComando.Connection = miConexion;
+            miComando.CommandType = CommandType.StoredProcedure;
+            miComando.CommandText = "dbo.insertar_contacto";
+
+            //creamos los objetos parametros para luego agregarlos
+            //al objeto SqlCommand
+            SqlParameter paramApellido = new SqlParameter();
+            paramApellido.SqlDbType = SqlDbType.VarChar;
+            paramApellido.ParameterName = "@ape";
+            paramApellido.Value = txtapellido.Text;
+
+            SqlParameter paramNombre = new SqlParameter();
+            paramNombre.SqlDbType = SqlDbType.VarChar;
+            paramNombre.ParameterName = "@nom";
+            paramNombre.Value = txtnombre.Text;
+
+            SqlParameter paramDni = new SqlParameter();
+            paramDni.SqlDbType = SqlDbType.Int;
+            paramDni.ParameterName = "@dni";
+            paramDni.Value =Convert.ToInt32(txtdni.Text);
+
+            //agrego los parametros al sqlcommand
+            miComando.Parameters.Add(paramApellido);
+            miComando.Parameters.Add(paramNombre);
+            miComando.Parameters.Add(paramDni);
+
+            //ejecuto el sqlcommand
+            miComando.ExecuteNonQuery();
+
+
+
+
+
+
+
+        }
+    }
+}
